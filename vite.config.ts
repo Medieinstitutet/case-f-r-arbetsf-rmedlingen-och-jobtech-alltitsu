8404c638a32ce5616366dae30d9c0564df24efbc
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-09-14 14:30:51 +0200

    📝 first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ["@digi/arbetsformedlingen"],
+  },
   plugins: [react()],
 })
