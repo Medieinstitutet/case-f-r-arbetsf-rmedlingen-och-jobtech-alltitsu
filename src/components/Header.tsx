d64905279fc3309395422b62c4ab5dddbb7c410a
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-09-15 13:12:08 +0200

    added sidemenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,62 @@
-import { DigiNavigationSidebarButton } from "@digi/arbetsformedlingen-react"
-import logo from "../assets/logo.png"
-import { HeaderWrapper } from "./styled/Wrappers"
+import {
+  DigiNavigationSidebar,
+  DigiNavigationSidebarButton,
+  DigiNavigationVerticalMenu,
+  DigiNavigationVerticalMenuItem,
+} from '@digi/arbetsformedlingen-react';
+import logo from '../assets/logo.png';
+import { HeaderWrapper } from './styled/Wrappers';
+import {
+  NavigationSidebarPosition,
+  NavigationSidebarVariation,
+  NavigationVerticalMenuVariation,
+} from '@digi/arbetsformedlingen';
+import { useState } from 'react';
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  return <>
-    <HeaderWrapper>
-      <img src={logo} alt="Alltisu logo." width={100} height={100} />
-      <DigiNavigationSidebarButton afText="Meny" />
-    </HeaderWrapper>
-  </>
-}
+  return (
+    <>
+      <HeaderWrapper>
+        <img src={logo} alt="Alltisu logo." width={100} height={100} />
+        <DigiNavigationSidebarButton afText="Meny" onClick={() => setMenuOpen(!menuOpen)} />
+        <DigiNavigationSidebar
+          afActive={menuOpen}
+          afStickyHeader={true}
+          afBackdrop={true}
+          afPosition={NavigationSidebarPosition.END}
+          afVariation={NavigationSidebarVariation.OVER}
+          afCloseButtonText="Close"
+          onAfOnClose={() => setMenuOpen(!menuOpen)}
+        >
+          <DigiNavigationVerticalMenu afVariation={NavigationVerticalMenuVariation.PRIMARY}>
+            <ul>
+              <li>
+                <DigiNavigationVerticalMenuItem
+                  afText="Home"
+                  afActive={true}
+                  af-href={'/'}
+                ></DigiNavigationVerticalMenuItem>
+              </li>
+              <li>
+                <DigiNavigationVerticalMenuItem
+                  afText="Search"
+                  afActive={true}
+                  af-href={'/search'}
+                ></DigiNavigationVerticalMenuItem>
+              </li>
+              <li>
+                <DigiNavigationVerticalMenuItem
+                  afText="Contact"
+                  afActive={true}
+                  af-href={'/contact'}
+                ></DigiNavigationVerticalMenuItem>
+              </li>
+            </ul>
+          </DigiNavigationVerticalMenu>
+        </DigiNavigationSidebar>
+      </HeaderWrapper>
+    </>
+  );
+};
--- src/components/styled/Wrappers.tsx
+++ src/components/styled/Wrappers.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
-  background-color: aqua;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background-color: #C2EFEB;
 `;
