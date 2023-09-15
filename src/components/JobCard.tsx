185a094a45dce6987e0a2bb9604b7cbb52ca24c5
tittiet <tittie.thomasson@hotmail.se>
2023-09-15 12:32:24 +0200

    add component from arbetsformedlingen to add styling

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,14 +1,17 @@
+import { DigiTypography } from '@digi/arbetsformedlingen-react';
 import { JobWrapper } from './styled/Wrappers';
+import { TypographyVariation } from '@digi/arbetsformedlingen';
 
 export const JobCard = () => {
   return (
     <JobWrapper>
-      <h3>Job title</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
+      <DigiTypography afVariation={TypographyVariation.SMALL}>
+        <h3>Job Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam...
+        </p>
+      </DigiTypography>
     </JobWrapper>
   );
 };
