0380b750a2abdbb40c625be95d8c0cec4c235b61
tittiet <tittie.thomasson@hotmail.se>
2023-09-15 15:22:03 +0200

    add button and feature to submit the form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,27 +7,36 @@ import {
 } from '@digi/arbetsformedlingen';
 import { DigiFormInput, DigiFormTextarea } from '@digi/arbetsformedlingen-react';
 import { ContactWrapper } from './styled/Wrappers';
+import { FormEvent } from 'react';
+
+const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault();
+  console.log('formuläret skickades');
+};
 
 export const ContactForm = () => {
   return (
     <ContactWrapper>
-      <DigiFormInput
-        afLabel="Namn:"
-        afVariation={FormInputVariation.MEDIUM}
-        afType={FormInputType.TEXT}
-        afValidation={FormInputValidation.NEUTRAL}
-      ></DigiFormInput>
-      <DigiFormInput
-        afLabel="Email:"
-        afVariation={FormInputVariation.MEDIUM}
-        afType={FormInputType.EMAIL}
-        afValidation={FormInputValidation.NEUTRAL}
-      ></DigiFormInput>
-      <DigiFormTextarea
-        afLabel="Etikett"
-        afVariation={FormTextareaVariation.MEDIUM}
-        afValidation={FormTextareaValidation.NEUTRAL}
-      ></DigiFormTextarea>
+      <form onSubmit={handleSubmit}>
+        <DigiFormInput
+          afLabel="Namn:"
+          afVariation={FormInputVariation.MEDIUM}
+          afType={FormInputType.TEXT}
+          afValidation={FormInputValidation.NEUTRAL}
+        ></DigiFormInput>
+        <DigiFormInput
+          afLabel="Email:"
+          afVariation={FormInputVariation.MEDIUM}
+          afType={FormInputType.EMAIL}
+          afValidation={FormInputValidation.NEUTRAL}
+        ></DigiFormInput>
+        <DigiFormTextarea
+          afLabel="Etikett"
+          afVariation={FormTextareaVariation.MEDIUM}
+          afValidation={FormTextareaValidation.NEUTRAL}
+        ></DigiFormTextarea>
+        <button>Skicka</button>
+      </form>
     </ContactWrapper>
   );
 };
