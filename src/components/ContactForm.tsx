baf41a54f5f3a0562e1cc087557270a4e39bf694
tittiet <tittie.thomasson@hotmail.se>
2023-09-17 20:58:40 +0200

    added a state to save userinfo and pass it when submitting

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,37 +5,87 @@ import {
   FormTextareaValidation,
   FormTextareaVariation,
 } from '@digi/arbetsformedlingen';
-import { DigiFormInput, DigiFormTextarea } from '@digi/arbetsformedlingen-react';
+import { DigiButton, DigiFormInput, DigiFormTextarea } from '@digi/arbetsformedlingen-react';
 import { ContactWrapper } from './styled/Wrappers';
-import { FormEvent } from 'react';
-
-const handleSubmit = (e: React.FormEvent) => {
-  e.preventDefault();
-  console.log('formuläret skickades');
-};
+import { useState } from 'react';
+import { ButtonSize, ButtonVariation } from '@digi/arbetsformedlingen';
+import {
+  DigiFormInputCustomEvent,
+  DigiFormTextareaCustomEvent,
+} from '@digi/arbetsformedlingen/dist/types/components';
 
 export const ContactForm = () => {
+  interface IContactForm {
+    username: string;
+    email: string;
+    message: string;
+  }
+
+  const [formData, setFormData] = useState<IContactForm>({
+    username: '',
+    email: '',
+    message: '',
+  });
+
+  const handleInput = (
+    e: DigiFormInputCustomEvent<string> | DigiFormTextareaCustomEvent<string>,
+  ) => {
+    e.preventDefault();
+    const name = e.target.afName;
+
+    setFormData({ ...formData, [name]: e.target.value });
+    console.log(formData);
+  };
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+    console.log('formuläret skickades' + JSON.stringify(formData));
+
+    setFormData({
+      username: '',
+      email: '',
+      message: '',
+    });
+  };
+
   return (
     <ContactWrapper>
-      <form onSubmit={handleSubmit}>
+      <form>
         <DigiFormInput
           afLabel="Namn:"
           afVariation={FormInputVariation.MEDIUM}
           afType={FormInputType.TEXT}
           afValidation={FormInputValidation.NEUTRAL}
+          afName="username"
+          afValue={formData.username}
+          onAfOnInput={handleInput}
         ></DigiFormInput>
         <DigiFormInput
           afLabel="Email:"
           afVariation={FormInputVariation.MEDIUM}
           afType={FormInputType.EMAIL}
           afValidation={FormInputValidation.NEUTRAL}
+          afName="email"
+          afValue={formData.email}
+          onAfOnInput={handleInput}
         ></DigiFormInput>
         <DigiFormTextarea
-          afLabel="Etikett"
+          afLabel="Meddelande:"
           afVariation={FormTextareaVariation.MEDIUM}
           afValidation={FormTextareaValidation.NEUTRAL}
+          afName="message"
+          afValue={formData.message}
+          onAfOnInput={handleInput}
         ></DigiFormTextarea>
-        <button>Skicka</button>
+        <DigiButton
+          afSize={ButtonSize.MEDIUM}
+          afVariation={ButtonVariation.PRIMARY}
+          afFullWidth={false}
+          className="alltitsuStyling"
+          onAfOnClick={handleSubmit}
+        >
+          Skicka
+        </DigiButton>
       </form>
     </ContactWrapper>
   );
