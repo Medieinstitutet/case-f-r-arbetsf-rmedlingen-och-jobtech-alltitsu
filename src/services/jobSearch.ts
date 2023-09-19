efa5a4730345695bdf1ede02ac1e6a6f2f1476da
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-09-19 13:56:28 +0200

    🐛 Restore getHistoricalData

--- src/services/jobSearch.ts
+++ src/services/jobSearch.ts
@@ -6,13 +6,13 @@ export const getHistoricalJobs = async (
   fromDate: string,
   toDate: string,
   employer: string,
-): Promise<IJobSearchResponse[]> => {
+): Promise<IJobSearchResponse> => {
   try {
     const BASEURL = `https://historical.api.jobtechdev.se/search`;
     const queryParams = `?employer=${employer}&offset=0&limit=10&request-timeout=300&historical-from=${fromDate}&historical-to=${toDate}`;
     const apiUrl = `${BASEURL}${queryParams}`;
 
-    const jobsResponse = await get<IJobSearchResponse[]>(apiUrl);
+    const jobsResponse = await get<IJobSearchResponse>(apiUrl);
 
     console.log(jobsResponse);
 
