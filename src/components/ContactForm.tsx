fffa2b60c71e6a8db3d758f61339f2d492724dda
tittieth <tittie.thomasson@medieinstitutet.se>
2023-09-19 22:21:18 +0200

    validation for all inputs and show errorcomponents

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -24,6 +24,10 @@ export const ContactForm = () => {
     message: '',
   });
 
+  const [error, setError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+
   const handleInput = (
     e: DigiFormInputCustomEvent<string> | DigiFormTextareaCustomEvent<string>,
   ) => {
@@ -34,21 +38,44 @@ export const ContactForm = () => {
     console.log(formData);
   };
 
+  const validateForm = (field: string) => {
+    console.log(error);
+
+    if (field === 'username') {
+      if (/^[a-zåäöA-ZÅÄÖ]+$/.test(formData.username)) {
+        console.log(error);
+        setError(false);
+      } else {
+        setError(true);
+        console.log(error);
+      }
+    } else if (field === 'email') {
+      if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email)) {
+        setEmailError(false);
+      } else {
+        setEmailError(true);
+      }
+    } else if (field === 'message') {
+      if (formData.message === '') {
+        setMessageError(true);
+      } else {
+        setMessageError(false);
+      }
+    }
+  };
+
   const handleSubmit = async (e: Event) => {
     e.preventDefault();
-    try {
-      if (
-        /^[a-zA-Z]+$/.test(formData.username) &&
-        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email) &&
-        formData.message.trim() !== ''
-      ) {
-        await submitForm(formData);
-      } else {
-        alert('Fyll i alla fält');
-        return;
-      }
-    } catch (error) {
-      console.log(error);
+    console.log('error' + error);
+    console.log('message' + messageError);
+    console.log('email' + emailError);
+
+    if (!error && !emailError && !messageError) {
+      await submitForm(formData);
+      console.log('formulär skickat');
+    } else {
+      console.log('fel i formuläret');
+      return;
     }
 
     setFormData({
@@ -61,32 +88,76 @@ export const ContactForm = () => {
   return (
     <ContactWrapper>
       <form>
-        <DigiFormInput
-          afLabel="Namn:"
-          afVariation={FormInputVariation.MEDIUM}
-          afType={FormInputType.TEXT}
-          afValidation={FormInputValidation.NEUTRAL}
-          afName="username"
-          afValue={formData.username}
-          onAfOnInput={handleInput}
-        ></DigiFormInput>
-        <DigiFormInput
-          afLabel="Email:"
-          afVariation={FormInputVariation.MEDIUM}
-          afType={FormInputType.EMAIL}
-          afValidation={FormInputValidation.NEUTRAL}
-          afName="email"
-          afValue={formData.email}
-          onAfOnInput={handleInput}
-        ></DigiFormInput>
-        <DigiFormTextarea
-          afLabel="Meddelande:"
-          afVariation={FormTextareaVariation.MEDIUM}
-          afValidation={FormTextareaValidation.NEUTRAL}
-          afName="message"
-          afValue={formData.message}
-          onAfOnInput={handleInput}
-        ></DigiFormTextarea>
+        {!error ? (
+          <DigiFormInput
+            afLabel="Namn:"
+            afVariation={FormInputVariation.MEDIUM}
+            afType={FormInputType.TEXT}
+            afValidation={FormInputValidation.NEUTRAL}
+            afName="username"
+            afValue={formData.username}
+            onAfOnInput={handleInput}
+            onAfOnFocusout={() => validateForm('username')}
+          ></DigiFormInput>
+        ) : (
+          <DigiFormInput
+            afLabel="Namn:"
+            afVariation={FormInputVariation.MEDIUM}
+            afType={FormInputType.TEXT}
+            afValidation={FormInputValidation.ERROR}
+            afValidationText="Endast bokstäver"
+            afValue={formData.username}
+            afName="username"
+            onAfOnInput={handleInput}
+            onAfOnFocusout={() => validateForm('username')}
+          ></DigiFormInput>
+        )}
+        {!emailError ? (
+          <DigiFormInput
+            afLabel="Email:"
+            afVariation={FormInputVariation.MEDIUM}
+            afType={FormInputType.EMAIL}
+            afValidation={FormInputValidation.NEUTRAL}
+            afName="email"
+            afValue={formData.email}
+            onAfOnInput={handleInput}
+            onAfOnFocusout={() => validateForm('email')}
+          ></DigiFormInput>
+        ) : (
+          <DigiFormInput
+            afLabel="Email:"
+            afVariation={FormInputVariation.MEDIUM}
+            afType={FormInputType.TEXT}
+            afValidation={FormInputValidation.ERROR}
+            afValidationText="Stämmer adressen?"
+            afValue={formData.email}
+            afName="email"
+            onAfOnInput={handleInput}
+            onAfOnFocusout={() => validateForm('email')}
+          ></DigiFormInput>
+        )}
+        {!messageError ? (
+          <DigiFormTextarea
+            afLabel="Meddelande:"
+            afVariation={FormTextareaVariation.MEDIUM}
+            afValidation={FormTextareaValidation.NEUTRAL}
+            afName="message"
+            afValue={formData.message}
+            onAfOnInput={handleInput}
+            onAfOnFocusout={() => validateForm('message')}
+          ></DigiFormTextarea>
+        ) : (
+          <DigiFormTextarea
+            afLabel="Meddelande:"
+            afVariation={FormTextareaVariation.MEDIUM}
+            afValidation={FormTextareaValidation.ERROR}
+            afValidationText="Fyll i ett meddelande"
+            afName="message"
+            afValue={formData.message}
+            onAfOnInput={handleInput}
+            onAfOnFocusout={() => validateForm('message')}
+          ></DigiFormTextarea>
+        )}
         <div className="contact-btn">
           <DigiButton
             afSize={ButtonSize.MEDIUM}
