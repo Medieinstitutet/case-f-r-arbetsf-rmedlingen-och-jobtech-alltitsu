101102a848aabeb194e4d0a6a05dc39f57935a5d
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-09-19 13:06:56 +0200

    added ad link button

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,21 +1,26 @@
-import { DigiTypography } from '@digi/arbetsformedlingen-react';
+import { DigiLinkInternal, DigiTypography } from '@digi/arbetsformedlingen-react';
 import { JobWrapper } from './styled/Wrappers';
-import { TypographyVariation } from '@digi/arbetsformedlingen';
+import { LinkVariation, TypographyVariation } from '@digi/arbetsformedlingen';
 import './JobCard.scss';
 
 interface JobCardProps {
   title: string;
   text: string;
   employer: string;
+  id: string;
 }
-export const JobCard: React.FC<JobCardProps> = ({ title, text, employer}) => {
+
+export const JobCard: React.FC<JobCardProps> = ({ title, text, employer, id }) => {
   return (
     <JobWrapper>
       <DigiTypography afVariation={TypographyVariation.SMALL}>
-        <h3>{title}</h3>
-        <p className='jobDescription'>{text}</p>
-        <h2>{employer}</h2>
+        <h4>{title}</h4>
+        <p>{employer}</p>
+        <p className="jobDescription">{text}</p>
       </DigiTypography>
+      <DigiLinkInternal afHref={`/${id}`} afVariation={LinkVariation.SMALL}>
+        Gå till annons
+      </DigiLinkInternal>
     </JobWrapper>
   );
 };
--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -1,14 +1,16 @@
-import { IJobSearchResponse } from "../models/IJobSearchResponse";
-import { SearchForm } from "./SearchForm";
-import SearchResult from "./SearchResult";
+import { IJobSearchResponse } from '../models/IJobSearchResponse';
+import { SearchForm } from './SearchForm';
+import SearchResult from './SearchResult';
 
 export const SearchPage = () => {
   const jobSearchResponse: IJobSearchResponse = {
-    total: {NumberOfHits: { value:0}},
-    hits:[],
+    total: { NumberOfHits: { value: 0 } },
+    hits: [],
   };
-  return <div>
-    <SearchForm></SearchForm>
-    <SearchResult jobSearchResponse={jobSearchResponse}></SearchResult>
-  </div>;
+  return (
+    <div>
+      <SearchForm></SearchForm>
+      <SearchResult jobSearchResponse={jobSearchResponse}></SearchResult>
+    </div>
+  );
 };
