8b0dd1c457552e634586714faf992e9ad2be896b
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-09-20 15:09:01 +0200

    🚧 Removed unused contexts

--- src/context/SearchContext.ts
+++ src/context/SearchContext.ts
@@ -1,8 +1,4 @@
 import { createContext } from 'react';
-
-export const ToDateContext = createContext('');
 export const SetToDateContext = createContext(() => {});
-export const FromDateContext = createContext('');
 export const SetFromDateContext = createContext(() => {});
-export const CompanyContext = createContext('');
 export const SetCompanyContext = createContext(() => {});
