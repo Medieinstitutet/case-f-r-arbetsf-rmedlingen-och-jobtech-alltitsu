a15416d4b1289dac31a1486cff836e8354973e7d
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-09-27 14:29:22 +0200

    bug test

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['@digi/arbetsformedlingen'],
-  },
   base: '/case-f-r-arbetsf-rmedlingen-och-jobtech-alltitsu/',
   plugins: [react()],
 });
